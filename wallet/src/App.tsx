8c859c0e8664e10eeda2123e8c9bb7ed3b41a6e7
Muhammad Hamza Khan <hamzah.hamlet@gmail.com>
2022-04-10 17:17:05 +0500

    cleanup

--- wallet/src/App.tsx
+++ wallet/src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-	return <div className="App"></div>;
-}
+const App = () => {
+	return <div className="App">App</div>;
+};
 
 export default App;
